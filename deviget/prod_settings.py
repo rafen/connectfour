8c299d1e067dd4b885d357157f8db2ad446493f1
rafen <capdevielle@gmail.com>
2016-11-18 18:04:32 -0300

    Added deployment configuration

--- deviget/prod_settings.py
+++ deviget/prod_settings.py
@@ -2,4 +2,21 @@ from .settings import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['http://rafen.webfactional.com/']
+ALLOWED_HOSTS = ['*']
+
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(os.path.dirname(__file__), '..', 'templates').replace('\\', '/')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
